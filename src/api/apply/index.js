e6766f7e810eede2d27dac02f016315842602f12
taherul <sktaherul@atilimited.net>
2024-08-29 12:04:03 +0530

    add getall apply route

--- src/api/apply/index.js
+++ src/api/apply/index.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
 import applyController from "../../controllers/apply.controller.js";
-
-
+import authGuard from "../../middlewares/authGuard.js";
 const router =Router();
 
 router.route("/").post( applyController.createApply);
+router.get('/apply', authGuard("ADMIN"), applyController.getAllApplies);
 export default router
--- src/controllers/apply.controller.js
+++ src/controllers/apply.controller.js
@@ -3,22 +3,24 @@ import ApiError from "../utils/ApiError.js";
 import catchAsync from "../utils/catchAsync.js";
 import sendResponse from "../utils/sendResponse.js";
 
+// Create Apply
 const createApply = catchAsync(async (req, res) => {
-  const { name, email, phoneNumber, message } = req.body; 
+  const { name, email, phoneNumber, message } = req.body;
 
   if (!name || !email || !message || !phoneNumber) {
     throw new ApiError(400, "Please provide all required fields");
   }
-  const isExistingUser = await Apply.findOne({ email });
 
+  const isExistingUser = await Apply.findOne({ email });
   if (isExistingUser) {
     throw new ApiError(409, "Email already exists");
   }
+
   const apply = await Apply.create({
     name,
     email,
     phoneNumber,
-    message, 
+    message,
   });
 
   sendResponse(res, {
@@ -28,8 +30,20 @@ const createApply = catchAsync(async (req, res) => {
   });
 });
 
+// Get All Applies
+const getAllApplies = catchAsync(async (req, res) => {
+  const applies = await Apply.find();
+
+  sendResponse(res, {
+    statusCode: 200,
+    message: "Fetched all applies successfully",
+    result: applies,
+  });
+});
+
 const applyController = {
   createApply,
+  getAllApplies,
 };
 
 export default applyController;
